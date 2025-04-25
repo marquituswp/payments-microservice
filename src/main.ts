83b3802da48dde40a52902e2e8b0242ac09eb689
Marcos Lopez <marcoslopezgarau@gmail.com>
2025-04-25 16:35:04 +0200

    refactor: improve microservice connection configuration for NATS transport

--- src/main.ts
+++ src/main.ts
@@ -19,12 +19,17 @@ async function bootstrap() {
     }),
   );
 
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.NATS,
-    options: {
-      servers: envs.natsServers,
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
     },
-  });
+    {
+      inheritAppConfig: true,
+    },
+  );
 
   await app.startAllMicroservices();
   logger.log('Payments-ms microservice is running');
